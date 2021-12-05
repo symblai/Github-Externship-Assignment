770d60e00abb84b48723b803b3582b43887e93ce
nithin88-ops <58203293+nithin88-ops@users.noreply.github.com>
2021-12-05 12:18:20 +0530

    updated

--- Solution.js
+++ Solution.js
@@ -1,1 +1,13 @@
 // Write your code here
+function findMissingDigit(equation){
+    var equationArray = equation.split("");
+    var firstNumber = equationArray[0];
+    var secondNumber = equationArray[2];
+    var missingDigit = "";
+    for(var i = 0; i < firstNumber.length; i++){
+        if(firstNumber[i] !== secondNumber[i]){
+            missingDigit = firstNumber[i];
+        }
+    }
+    return missingDigit;
+}
