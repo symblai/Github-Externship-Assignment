55c619bfc5b1b4a3b63f9cb9e6bd20aaaeee3708
nithin88-ops <58203293+nithin88-ops@users.noreply.github.com>
2021-12-05 13:46:19 +0530

    updated

--- Solution.js
+++ Solution.js
@@ -1,23 +1,18 @@
+// Write your code here
+
 class FixEquation{
     findMissingDigit(equation){
         var equationArray = equation.split(" ");
         var pos,result;
-        //var equationArray = equationArray1;
         for(var i in equationArray){
             if(equationArray[i].indexOf("?") == -1){
-                if(i%2==0){
-                    equationArray[i] = parseInt(equationArray[i]);
-                }
+                if(i%2==0) equationArray[i] = parseInt(equationArray[i]);
             }
-            else {
-                pos = i;
-                console.log("true" , equationArray[i],typeof(equationArray[i]));
-            }
+            else pos = i;
         }
         if(pos == 0){
             result = (equationArray[6]-equationArray[4])/equationArray[2];
             result = result.toString();
-            
         }
         else if(pos == 2){
             result = (equationArray[6]-equationArray[4])/equationArray[0];
@@ -31,13 +26,8 @@ class FixEquation{
             result = equationArray[4]+(equationArray[0] * equationArray[2]);
             result = result.toString();
         }
-        if(result.length != equationArray[pos].length) {
-                return -1;
-            }
-            else {
-                return result[equationArray[pos].indexOf("?")]
-            }
-        //console.log(result);
-        
+
+        if(result.length != equationArray[pos].length) return -1;
+        else return result[equationArray[pos].indexOf("?")]        
     }
 }
