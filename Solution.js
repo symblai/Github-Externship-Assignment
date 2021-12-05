4b486985c3433e62e2994b8f3367e393bcebde13
nithin88-ops <58203293+nithin88-ops@users.noreply.github.com>
2021-12-05 13:40:06 +0530

    updated

--- Solution.js
+++ Solution.js
@@ -1,41 +1,43 @@
-function findMissingDigit(equation){
-    var equationArray = equation.split(" ");
-    var pos,result;
-    //var equationArray = equationArray1;
-    for(var i in equationArray){
-        if(equationArray[i].indexOf("?") == -1){
-            if(i%2==0){
-                equationArray[i] = parseInt(equationArray[i]);
+class FixEquation{
+    findMissingDigit(equation){
+        var equationArray = equation.split(" ");
+        var pos,result;
+        //var equationArray = equationArray1;
+        for(var i in equationArray){
+            if(equationArray[i].indexOf("?") == -1){
+                if(i%2==0){
+                    equationArray[i] = parseInt(equationArray[i]);
+                }
             }
-            console.log(equationArray[i]);
+            else {
+                pos = i;
+                console.log("true" , equationArray[i],typeof(equationArray[i]));
+            }
         }
-        else {
-            pos = i;
-            console.log("true" , equationArray[i]);
+        if(pos == 0){
+            result = (equationArray[6]-equationArray[4])/equationArray[2];
+            result = result.toString();
+            
         }
+        else if(pos == 2){
+            result = (equationArray[6]-equationArray[4])/equationArray[0];
+            result = result.toString();
+        }
+        else if(pos==4){
+            result = equationArray[6]-(equationArray[0] * equationArray[2]);
+            result = result.toString();
+        }
+        else if(pos==6){
+            result = equationArray[4]+(equationArray[0] * equationArray[2]);
+            result = result.toString();
+        }
+        if(result.length != equationArray[pos].length) {
+                return -1;
+            }
+            else {
+                return result[equationArray[pos].indexOf("?")]
+            }
+        //console.log(result);
+        
     }
-    if(pos == 0){
-        result = (equationArray[6]-equationArray[4])/equationArray[2];
-    }
-    else if(pos == 2){
-        result = (equationArray[6]-equationArray[4])/equationArray[0];
-    }
-    else if(pos==4){
-        result = equationArray[6]-(equationArray[0] * equationArray[2]);
-    }
-    else if(pos==6){
-        result = equationArray[4]+(equationArray[0] * equationArray[2]);
-    }
-    console.log(result);
-    
 }
-console.log(findMissingDigit("42 * 47 + 2 = 1?76"))
-console.log("\n");
-console.log(findMissingDigit("4? * 47 + 2 = 1976"))
-console.log("\n");
-console.log(findMissingDigit("42 * ?7 + 2 = 1976"))
-console.log("\n");
-console.log(findMissingDigit("42 * ?47 + 2 = 1976"))
-console.log("\n");
-console.log(findMissingDigit("2 * 12? + 2 = 247"))
-console.log("\n");
