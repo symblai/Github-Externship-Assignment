ef6967e5b917e7f74c89fb983a1aac57c7099036
TASNEEM KOUSHAR <imatasneemkoushar@gmail.com>
2021-12-06 23:30:36 +0530

    Update Solution.js

--- Solution.js
+++ Solution.js
@@ -1,1 +1,23 @@
 // Write your code here
+class FixEquation {
+  findMissingDigit(equation) {
+    const operands = equation.split(/\*|\+|=/).map((n) => n.trim());
+    const [a, b, c, d] = operands.map(Number);
+
+    const unknownOperands = operands.findIndex((opernd) => !Number(opernd));
+    const unknownOperandsOperations = ["(d-c)/b", "(d-c)/a", "d-a*b", "a*b+c"][unknownOperands];
+
+    const evaluation = eval(unknownOperandsOperations);
+    if (evaluation < 0 || !Number.isInteger(evaluation)){
+      return -1;
+    }
+
+    const value = operands[unknownOperands];
+
+    const myRegex = value.replace("?", "(.?)");
+    const solvedNum = String(evaluation).match(myRegex);
+
+    const ansVal = (solvedNum || [])[1];
+    return Number(ansVal) ?? -1;
+  }
+}
