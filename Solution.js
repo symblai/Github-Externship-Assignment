7522d5673b2557bc796f0c7177639ec1b4bab51b
Nithin Shanmugam V <58203293+NithinShanmugamV@users.noreply.github.com>
2021-12-07 12:28:52 +0530

    Updated Solution.js

--- Solution.js
+++ Solution.js
@@ -1,33 +1,32 @@
 // Write your code here
 
-class FixEquation{
-    findMissingDigit(equation){
-        var equationArray = equation.split(" ");
-        var pos,result;
-        for(var i in equationArray){
-            if(equationArray[i].indexOf("?") == -1){
-                if(i%2==0) equationArray[i] = parseInt(equationArray[i]);
-            }
-            else pos = i;
-        }
-        if(pos == 0){
-            result = (equationArray[6]-equationArray[4])/equationArray[2];
-            result = result.toString();
-        }
-        else if(pos == 2){
-            result = (equationArray[6]-equationArray[4])/equationArray[0];
-            result = result.toString();
-        }
-        else if(pos==4){
-            result = equationArray[6]-(equationArray[0] * equationArray[2]);
-            result = result.toString();
-        }
-        else if(pos==6){
-            result = equationArray[4]+(equationArray[0] * equationArray[2]);
-            result = result.toString();
-        }
-
-        if(result.length != equationArray[pos].length) return -1;
-        else return result[equationArray[pos].indexOf("?")]        
+class FixEquation {
+  findMissingDigit(equation) {
+    let equation1 = equation.split(/[+,*,=,]/);
+    let pos, result;
+    for (let i in equation1) {
+      equation1[i] = equation1[i].trim();
+      if (equation1[i].indexOf("?") == -1) {
+        equation1[i] = parseInt(equation1[i]);
+      } else pos = i;
     }
+    if (pos == 0) {
+      result = (equation1[3] - equation1[2]) / equation1[1];
+      result = result.toString();
+    } else if (pos == 1) {
+      result = (equation1[3] - equation1[2]) / equation1[0];
+      result = result.toString();
+    } else if (pos == 2) {
+      result = equation1[3] - equation1[0] * equation1[1];
+      result = result.toString();
+    } else if (pos == 3) {
+      result = equation1[2] + equation1[0] * equation1[1];
+      result = result.toString();
+    }
+    //console.log(equation1);
+    if (result.length != equation1[pos].length) return -1;
+    else return result[equation1[pos].indexOf("?")];
+  }
 }
+
+//code written by Nithin Shanmugam V
