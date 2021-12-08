ee7d24605499ca6846d1c012e024ac583e5df80a
singhdipesh14 <58405253+singhdipesh14@users.noreply.github.com>
2021-12-08 19:36:00 +0530

    feat: Add methods for evaluation
    Add methods for each terminal (A, B, C, and D, which contains '?')
    which evaluate the expression except that terminal and equate it
    to the terminal's value and check if the missing value can be found.
    
    Steps for evaluation :
     - Extract the values of terminals from the class member 'values'
     - Evaluate the remaining expression
     - Compare the lengths of the evaluated string and the terminal string
     - If same length, return number at index of '?' in terminal
     - Else return -1

--- Solution.js
+++ Solution.js
@@ -24,16 +24,81 @@ class FixEquation {
 		this.values = { ...values }
 	}
 
+	evaluateA = () => {
+		const [tempA, B, C, D] = Object.values(this.values).map(item => parseInt(item))
+		const solvedExpn = (D - C) / B
+		if (solvedExpn % 1 !== 0) {
+			return -1
+		}
+		const stringSolvedExpn = solvedExpn.toString()
+		if (stringSolvedExpn.length === this.values.A.length) {
+			const indexMissingDigit = this.values.A.indexOf('?')
+			return parseInt(stringSolvedExpn[indexMissingDigit])
+		}
+		return -1
+
+	}
+
+	evaluateB = () => {
+		const [A, tempB, C, D] = Object.values(this.values).map(item => parseInt(item))
+		const solvedExpn = (D - C) / A
+		if (solvedExpn % 1 !== 0) {
+			return -1
+		}
+		const stringSolvedExpn = solvedExpn.toString()
+		if (stringSolvedExpn.length === this.values.B.length) {
+			const indexMissingDigit = this.values.B.indexOf('?')
+			return parseInt(stringSolvedExpn[indexMissingDigit])
+		}
+		return -1
+
+	}
+
+	evaluateC = () => {
+		const [A, B, tempC, D] = Object.values(this.values).map(item => parseInt(item))
+		const solvedExpn = D - A * B
+		const stringSolvedExpn = solvedExpn.toString()
+		if (stringSolvedExpn.length === this.values.C.length) {
+			const indexMissingDigit = this.values.C.indexOf('?')
+			return parseInt(stringSolvedExpn[indexMissingDigit])
+		}
+		return -1
+
+	}
+
+	evaluateD = () => {
+		const [A, B, C, tempD] = Object.values(this.values).map(item => parseInt(item))
+		const solvedExpn = A * B + C
+		const stringSolvedExpn = solvedExpn.toString()
+		if (stringSolvedExpn.length === this.values.D.length) {
+			const indexMissingDigit = this.values.D.indexOf('?')
+			return parseInt(stringSolvedExpn[indexMissingDigit])
+		}
+		return -1
+
+	}
+
 	findMissingDigit = (equation) => {
 		this.equation = equation
 		this.splitAtEqual()
 		this.assignValues()
-		console.log("lhs : " + this.lhs)
-		console.log("rhs : " + this.rhs)
-		console.log(this.values)
+		const { A, B, C, D } = this.values
+		if (A.includes('?')) {
+			return this.evaluateA()
+		}
+		else if (B.includes('?')) {
+			return this.evaluateB()
+		}
+		else if (C.includes('?')) {
+			return this.evaluateC()
+		}
+		else if (D.includes('?')) {
+			return this.evaluateD()
+		}
+		return -1
 	}
 }
 
 const solver = new FixEquation()
 
-solver.findMissingDigit("A? * B + C = D")
+console.log(solver.findMissingDigit("2 * 12? + 2 = 247"))
