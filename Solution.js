a9b0e838c95adb8ed2423b517cec6011a046df3e
singhdipesh14 <58405253+singhdipesh14@users.noreply.github.com>
2021-12-08 19:09:26 +0530

    feat: Add functions and members of FixEquation
    Add 4 class members to store intermediate augmented equation and values
    
    Add splitAtEqual method to split equation into RHS and LHS
    
    Add assignValues method to assign class members the appropriate value
    from the equation i.e. A, B, C, and D.

--- Solution.js
+++ Solution.js
@@ -1,10 +1,39 @@
 
 class FixEquation {
+	equation = ""
+
+	lhs = ""
+	rhs = ""
+
+	values = {
+		A: "",
+		B: "",
+		C: "",
+		D: ""
+	}
+
+	splitAtEqual = () => {
+		[this.lhs, this.rhs] = this.equation.split("=").map(item => item.trim())
+	}
+
+	assignValues = () => {
+		const D = this.rhs
+		const [A, restAfterA] = this.lhs.split("*").map(item => item.trim())
+		const [B, C] = restAfterA.split("+").map(item => item.trim())
+		const values = { A, B, C, D }
+		this.values = { ...values }
+	}
+
 	findMissingDigit = (equation) => {
-		console.log(equation)
+		this.equation = equation
+		this.splitAtEqual()
+		this.assignValues()
+		console.log("lhs : " + this.lhs)
+		console.log("rhs : " + this.rhs)
+		console.log(this.values)
 	}
 }
 
 const solver = new FixEquation()
 
-solver.findMissingDigit("A * B? + C = D")
+solver.findMissingDigit("A? * B + C = D")
