6df71caab3d2e1dc82344982032cc2d681a08d21
Roshan Kumar Sahu <roshankumar.sahu1999@gmail.com>
2021-12-10 02:48:13 +0530

    feat: Add FixEquation in Solution for Assignment
    The FixEquation class has four functions:
    1)function getVariables(equation): Retrieves A,B,C,D from equation.
    parameters: String(equation)
    return: Array([A,B,C,D])
    2)function getMissingNumberPosition(variables): Finds variable with missing digit in Array returned by function getVariables.
    parameters: Array[A,B,C,d]
    return: Integer(Position)
    3)function getMissingNumber(variables,index): Finds the real Number for the variable with "?".
    parameters: Array[A,B,C,D],Integer(Position from getMissingNumberPosition)
    return: Integer(Number)
    4)findMissingDigit(equation): Finds the digit to replace "?".
    parameters: String(equation)
    return: Integer(Digit(If Possible)/-1)

--- Solution.js
+++ Solution.js
@@ -1,1 +1,51 @@
 // Write your code here
+class FixEquation {
+  getVariables(equation) {
+    const temp = equation.split(/[+,*,=,]/);
+    return [temp[0].trim(), temp[1].trim(), temp[2].trim(), temp[3].trim()];
+  }
+  findMissingNumberPosition(variables) {
+    return variables.findIndex((element) => {
+      return element.includes("?");
+    });
+  }
+  getMissingNumber(variables, index) {
+    if (index === 3) {
+      return (
+        parseInt(variables[0]) * parseInt(variables[1]) + parseInt(variables[2])
+      );
+    } else if (index === 2) {
+      return (
+        parseInt(variables[3]) - parseInt(variables[0]) * parseInt(variables[1])
+      );
+    } else if (index == 1) {
+      return (
+        (parseInt(variables[3]) - parseInt(variables[2])) /
+        parseInt(variables[0])
+      );
+    } else {
+      return (
+        (parseInt(variables[3]) - parseInt(variables[2])) /
+        parseInt(variables[1])
+      );
+    }
+  }
+  findMissingDigit(equation) {
+    const variables = this.getVariables(equation);
+    const indexOfVariable = this.findMissingNumberPosition(variables);
+    const variable = this.getMissingNumber(
+      variables,
+      indexOfVariable
+    ).toString();
+    const missingDigitVariable = variables[indexOfVariable];
+    if (variable.length !== missingDigitVariable.length) {
+      return -1;
+    }
+    const indexOfDigit = missingDigitVariable.indexOf("?");
+    return parseInt(variable[indexOfDigit]);
+  }
+}
+
+module.exports = {
+  FixEquation,
+};
