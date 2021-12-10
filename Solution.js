0df07daca7967cbf6e9e468a7cd357d8a98d5a6a
aaditya9899 <aadityaprasad9899@gmail.com>
2021-12-10 11:11:09 +0530

    Check for question mark and and created a new variable original string to check missing digit later on

--- Solution.js
+++ Solution.js
@@ -9,9 +9,20 @@ class FixEquation {
         let B = list[1].trim();
         let C = list[2].trim();
         let D = list[3].trim();
-        if (!list && list.length<4) return -1;
 
-        
+        if (!list && list.length<4) {
+            return -1;
+        }
+        if (D.includes('?')) {
+            let a = parseInt(A);
+            let b = parseInt(B);
+            let c = parseInt(C);
+            const result = a * b + c;
+            outputString = result.toString();
+            desiredString = D;
+        } 
 }
+
 }
+
 
