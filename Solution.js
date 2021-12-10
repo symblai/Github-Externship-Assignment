5562ca9c9da4b9fb6aaee893f4c901c9a4ef0296
aaditya9899 <aadityaprasad9899@gmail.com>
2021-12-10 11:02:07 +0530

    extracting every variable from the expression

--- Solution.js
+++ Solution.js
@@ -1,1 +1,17 @@
-// Write your code here
+class FixEquation {
+
+    findMissingDigit(equation) {
+        if (!equation) return -1;
+
+        const list = equation.split(/[+,*,=,]/);
+
+        let A = list[0].trim();
+        let B = list[1].trim();
+        let C = list[2].trim();
+        let D = list[3].trim();
+        if (!list && list.length<4) return -1;
+
+        
+}
+}
+
