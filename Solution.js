5d155f033e2b7bdc607f9d1caa2dc64f9f19fb54
AISHWARYA CHANDRAKANT DIXIT <68919665+AishwaryaDixit815@users.noreply.github.com>
2021-12-17 16:34:41 +0530

    Feat: Implemented FixEquation class in Solution.js
    created class FixEquation with findMissingData function
    
    The functions of findMissingData : 
    1. accepts the equation string
    2. splits the string with respect to space character and stores the elements in an array
    3. finds the element having '?' character and stores it in a variable
    4. extracts the integer part of remaining elements into the respective variables
    5. computes the value of answer using the extracted integers
    6. if the answer is negative or floating or having trailing zeroes, returns -1
    7. else finds out the digit in place of '?' and returns the same
    
    inputted equation string from user
    created object of class FixEquation
    called the findMissing data using the object and passed the input string as parameter
    outputted the returned result

--- Solution.js
+++ Solution.js
@@ -1,1 +1,64 @@
 // Write your code here
+const readline = require("readline");
+
+const interface = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+class FixEquation{
+    findMissingDigit(equation){
+        const myArray = equation.split(" ");
+        var question;
+        var answer;
+
+        if(myArray[0].includes('?')){
+            question = myArray[0];
+            var B = parseInt(myArray[2]);
+            var C = parseInt(myArray[4]);
+            var D = parseInt(myArray[6]);
+            answer = (D-C)/B;
+        }
+        else if(myArray[2].includes('?')){
+            question = myArray[2];
+            var A = parseInt(myArray[0]);
+            var C = parseInt(myArray[4]);
+            var D = parseInt(myArray[6]);
+            answer = (D-C)/A;
+        }
+        else if(myArray[4].includes('?')){
+            question = myArray[4];
+            var A = parseInt(myArray[0]);
+            var B = parseInt(myArray[2]);
+            var D = parseInt(myArray[6]);
+            answer = D-(A*B);
+        }
+        else if(myArray[6].includes('?')){
+            question = myArray[6];
+            var A = parseInt(myArray[0]);
+            var B = parseInt(myArray[2]);
+            var C = parseInt(myArray[4]);
+            answer = (A*B)+C;
+        }
+
+        if(answer<0 || answer % 1 !== 0 || answer.toString().length < question.length){
+            return -1;
+        }
+        else{
+            answer = answer.toString();
+            for(let i=0; i<question.length; i++){
+                if(question[i]=='?'){
+                    return answer[i];
+                }
+            }
+        }
+    }
+}
+
+interface.question("Enter the equation string : ", function (equ) {
+    var object = new FixEquation();
+    const result = object.findMissingDigit(equ);
+    console.log(`Answer : ${result}`);
+    interface.close();
+});
+
