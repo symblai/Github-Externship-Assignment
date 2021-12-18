fd81321f8f29817aaa686e405ef655ad8a1adbb0
Surya Kumar Sahani <60179206+Surya742@users.noreply.github.com>
2021-12-18 11:25:57 +0530

    Github Externship Assignment Solution - Surya Kumar Sahani
    My approach:
    1. Split the equation string at every space.
    2. Filter out the A, B, C & D from the equation which was in the formate of A * B + C = D.
    3. Initialise two variables: elementWithMark & result, which will be getting compared at the end to find out the final answer.
    4. Store the element with the '?' mark in the variable elementWithMark, and compute the rest of the equation -> store its result in variable result.
    5. Check if the lengths of elementWithMark & result are the same, if not then return -1.
    6. Iterate over each character of the result:
        (I) if we get an index where the character of result and elementWithMark isn't matching plus it's not a '?' too, return -1.
        (II) on getting the index of '?' in elementWithMark, return the character from the computed result on the corresponding index.
    7. By default return -1 (if every check fails).

--- Solution.js
+++ Solution.js
@@ -1,1 +1,44 @@
 // Write your code here
+class FixEquation {
+    constructor(a) {
+        this.equation = a;
+    }
+    findMissingDigit() {
+        var eqn = this.equation.split(' '); //split the string on space
+        var A = eqn[0];
+        var B = eqn[2];
+        var C = eqn[4];
+        var D = eqn[6];
+        var elementWithMark;
+        var result;
+
+        if(D.includes('?')){ //if D contains '?'
+            result = parseInt(A) * parseInt(B) + parseInt(C);
+            elementWithMark = D;
+        }else if(A.includes('?')){ //if A contains '?'
+            result = (parseInt(D)-parseInt(C))/parseInt(B);
+            elementWithMark = A;
+        }else if(B.includes('?')){ //if B contains '?'
+            result = (parseInt(D)-parseInt(C))/parseInt(A);
+            elementWithMark = B;
+        }else if(C.includes('?')){ //if C contains '?'
+            result = (parseInt(D)-(parseInt(A)*parseInt(B)));
+            elementWithMark = C;
+        }
+        result = String(result); // change result type back to string
+        if(result.length !== elementWithMark.length){ // if the length of computed answer and the one with '?' isn't same, then return -1
+            return -1;
+        }
+        for(var i = 0; i < result.length; i++) {
+            if(result[i] !== elementWithMark[i] && elementWithMark[i] !== '?') { // if we get a index where the character of result and elementWithMark isn't matching plus it's not a '?' too
+                return -1;
+            }else if(elementWithMark[i] === '?'){ // on getting the index of '?', return the charcter from computed result on the corresponding index
+                return parseInt(result[i]);
+            }
+        }
+        return -1; // if every check fails, return -1
+    }
+}
+let magic = new FixEquation('42 * 47 + 2 = 1?76');
+let num = magic.findMissingDigit();
+console.log(num);
